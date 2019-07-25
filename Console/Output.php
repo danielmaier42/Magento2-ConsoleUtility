ebd5bdcbc36859b4d1f7b52fbbc7577044a545db
Daniel Maier <dm@bergwerk.ag>
2019-07-25 10:50:58 +0200

    ✨ add logger interface compatibility

--- Console/Output.php
+++ Console/Output.php
@@ -3,12 +3,13 @@
 namespace DanielMaier\ConsoleUtility\Console;
 
 use Magento\Framework\Exception\AbstractAggregateException;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Formatter\OutputFormatterInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Zend\Log\Logger;
 use Zend\Log\Writer\Stream;
 
-class Output implements OutputInterface
+class Output implements OutputInterface, LoggerInterface
 {
     /**
      * @var bool
@@ -240,6 +241,11 @@ class Output implements OutputInterface
         }
     }
 
+    public function logNotice($message)
+    {
+        $this->writeln('<notice>' . $message . '</notice>');
+    }
+
     public function logSuccess($message)
     {
         $this->writeln('<info>' . $message . '</info>');
@@ -315,5 +321,134 @@ class Output implements OutputInterface
     public function logEmpty()
     {
         return $this->logInfo('');
+    }
+
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function emergency($message, array $context = [])
+    {
+        $this->logError($message);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function alert($message, array $context = [])
+    {
+        $this->logError($message);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function critical($message, array $context = [])
+    {
+        $this->logError($message);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function error($message, array $context = [])
+    {
+        $this->logError($message);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function warning($message, array $context = [])
+    {
+        $this->logNotice($message);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function notice($message, array $context = [])
+    {
+        $this->logNotice($message);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function info($message, array $context = [])
+    {
+        $this->logInfo($message);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function debug($message, array $context = [])
+    {
+        $this->logInfo($message);
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     *
+     * @return void
+     */
+    public function log($level, $message, array $context = [])
+    {
+        $this->logInfo($message);
     }
 }
